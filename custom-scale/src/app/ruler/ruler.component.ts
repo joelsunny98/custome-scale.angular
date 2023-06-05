d4a4c938f4e543379de5166e3650ff169bed9efa
joel.sunny <joel.sunny@sreegcloudgmail.onmicrosoft.com>
2023-06-05 16:37:05 +0530

    Added Ruler Component

---
+++ custom-scale/src/app/ruler/ruler.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ruler',
+  templateUrl: './ruler.component.html',
+  styleUrls: ['./ruler.component.css']
+})
+export class RulerComponent {
+
+}
